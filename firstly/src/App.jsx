a5823a16697b700fd3b38fe5f98b7c8fb36ce665
MalgorzataVictor <malgorzatavictor@gmail.com>
2025-11-18 22:03:34 +0000

    Mongo lab Integrated Tasky React App

--- firstly/src/App.jsx
+++ firstly/src/App.jsx
@@ -1,92 +1,90 @@
 import './App.css';
 import Task from './components/Task';
-import React, { useState } from 'react';
 import AddTaskForm from './components/Form';
-import { v4 as uuidv4 } from 'uuid';
-
+import React, { useState, useEffect } from 'react';
+import {getTasks, addTask, deleteTask, updateTask} from "./api/tasky-api";
 
 function App() {
-  const [taskState, setTaskState] = useState({
-    tasks: [
-      { id: 1, title: "Dishes", description: "Empty dishwasher", deadline: "Today", priority: "Low", done: false },
-      { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow", priority: "Medium", done: false },
-      { id: 3, title: "Tidy up", deadline: "Today", priority: "High", done: false }
-    ]
-  });
+  
+  const [ taskState, setTaskState ] = useState({tasks: []});
 
- 
-  const [formState, setFormState] = useState({
-    title: "",
-    description: "",
-    deadline: "",
-    priority:""
-  });
+  useEffect(() => {
+      getTasks().then(tasks => {
+        setTaskState({tasks: tasks});
+      });
+    }, []);	
+  
+    const [ formState, setFormState ] = useState({
+      title: "",
+      description: "",
+      deadline: "",
+      priority: "Low"
+    });
+  
+  //console.log(formState);
 
   const doneHandler = (taskIndex) => {
     const tasks = [...taskState.tasks];
     tasks[taskIndex].done = !tasks[taskIndex].done;
-    setTaskState({ tasks });
-  };
+    updateTask(tasks[taskIndex]);
+    setTaskState({tasks});
+  }
 
   const deleteHandler = (taskIndex) => {
     const tasks = [...taskState.tasks];
+    const id=tasks[taskIndex]._id;
     tasks.splice(taskIndex, 1);
-    setTaskState({ tasks });
-  };
+    deleteTask(id);
+    setTaskState({tasks});
+  }
 
   const formChangeHandler = (event) => {
-    let form = { ...formState };
+    let form = {...formState};
 
-    switch (event.target.name) {
+    switch(event.target.name) {
       case "title":
-        form.title = event.target.value;
-        break;
+          form.title = event.target.value;
+          break;
       case "description":
-        form.description = event.target.value;
-        break;
+          form.description = event.target.value;
+          break;
       case "deadline":
-        form.deadline = event.target.value;
-        break;
-       case "priority":
-        form.priority = event.target.value;
-        break; 
+          form.deadline = event.target.value;
+          break;
+      case "priority":
+          form.priority = event.target.value;
+          break;      
       default:
-        break;
+          form = formState;
     }
     setFormState(form);
-  };
+  }
 
-
-    const formSubmitHandler = (event) => {
+  const formSubmitHandler = async (event) => {
     event.preventDefault();
-
-    const tasks = [...taskState.tasks];
+    const tasks = taskState.tasks?[...taskState.tasks]:[];
     const form = {...formState};
-
-    form.id = uuidv4();
-    
-    tasks.push(form);
+    const newTask = await addTask(form);
+    tasks.push(newTask);
     setTaskState({tasks});
   }
   
-
-
   return (
     <div className="container">
       <h1>Tasky</h1>
-      {taskState.tasks.map((task, index) => (
-        <Task
+      {taskState.tasks.map((task, index) => (              
+        <Task 
           title={task.title}
           description={task.description}
           deadline={task.deadline}
-          priority={task.priority}
-          key={task.id}
+          key={task._id}
           done={task.done}
-          deleteTask={() => deleteHandler(index)}
+          priority={task.priority}
           markDone={() => doneHandler(index)}
+          deleteTask = {() => deleteHandler(index)}
         />
       ))}
-        <AddTaskForm submit={formSubmitHandler} change={formChangeHandler} />
+      <AddTaskForm submit={formSubmitHandler} change={formChangeHandler} />
 
     </div>
   );
